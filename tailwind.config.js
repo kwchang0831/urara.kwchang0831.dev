fdb049a1df6cef47388ef7f85773141d2ddb3020
kwchang0831 <88667293+kwchang0831@users.noreply.github.com>
2022-05-01 17:54:20 +0800

    fix styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,26 +9,26 @@ export default /** @type {import('tailwindcss/tailwind-config').TailwindConfig} 
   plugins: [typography, daisyui],
   daisyui: {
     themes: [
-      // {
-      //   default: {
-      //     primary: '#1C4E80',
-      //     'primary-content': '#ECEFF4',
-      //     secondary: '#7C909A',
-      //     'secondary-content': '#ECEFF4',
-      //     accent: '#EA6947',
-      //     'accent-content': '#ECEFF4',
-      //     neutral: '#3B4252',
-      //     'neutral-content': '#D8DEE9',
-      //     'base-100': '#212121',
-      //     'base-200': '#1c1c1c',
-      //     'base-300': '#1a1a1a',
-      //     'base-content': '#e7edef',
-      //     info: '#0091D5',
-      //     success: '#6BB187',
-      //     warning: '#DBAE59',
-      //     error: '#AC3E31'
-      //   }
-      // },
+      {
+        default: {
+          primary: '#1C4E80',
+          'primary-content': '#ECEFF4',
+          secondary: '#7C909A',
+          'secondary-content': '#ECEFF4',
+          accent: '#EA6947',
+          'accent-content': '#ECEFF4',
+          neutral: '#3B4252',
+          'neutral-content': '#D8DEE9',
+          'base-100': '#212121',
+          'base-200': '#1c1c1c',
+          'base-300': '#1a1a1a',
+          'base-content': '#e7edef',
+          info: '#0091D5',
+          success: '#6BB187',
+          warning: '#DBAE59',
+          error: '#AC3E31'
+        }
+      },
       'business',
       'light',
       'dark',
