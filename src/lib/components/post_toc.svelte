28ab3b5e29b9199fbeddefc3253ade2051cb6dcd
kwchang0831 <88667293+kwchang0831@users.noreply.github.com>
2022-05-04 19:05:26 +0800

    add post_toc mouseDown to scroll

--- src/lib/components/post_toc.svelte
+++ src/lib/components/post_toc.svelte
@@ -10,6 +10,7 @@
   let intersecting: string[] = []
   let intersectingArticle: boolean = true
   let bordered: string[] = []
+  let pos = { top: 0, left: 0, x: 0, y: 0 }
 
   onMount(() => {
     if (window.screen.availWidth >= 1280) {
@@ -27,6 +28,42 @@
         headingsObserver.observe(element)
       )
       articleObserver.observe(document.getElementsByTagName('main')[0])
+
+      const post_toc = document.getElementById('post-toc')
+      post_toc.style.cursor = 'grab'
+      let pos = { top: 0, left: 0, x: 0, y: 0 }
+
+      const mouseDownHandler = function (e) {
+        post_toc.style.cursor = 'grabbing'
+        post_toc.style.userSelect = 'none'
+
+        pos = {
+          left: post_toc.scrollLeft,
+          top: post_toc.scrollTop,
+          x: e.clientX,
+          y: e.clientY
+        }
+
+        document.addEventListener('mousemove', mouseMoveHandler)
+        document.addEventListener('mouseup', mouseUpHandler)
+      }
+
+      const mouseMoveHandler = function (e) {
+        const dx = e.clientX - pos.x
+        const dy = e.clientY - pos.y
+        post_toc.scrollTop = pos.top + dy
+        post_toc.scrollLeft = pos.left - dx
+      }
+
+      const mouseUpHandler = function () {
+        post_toc.style.cursor = 'grab'
+        post_toc.style.removeProperty('user-select')
+
+        document.removeEventListener('mousemove', mouseMoveHandler)
+        document.removeEventListener('mouseup', mouseUpHandler)
+      }
+
+      post_toc.addEventListener('mousedown', mouseDownHandler)
     }
   })
 
