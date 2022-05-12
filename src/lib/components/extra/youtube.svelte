8e27b2946e6f13eff3a42560ec1c159611b88543
kwchang0831 <88667293+kwchang0831@users.noreply.github.com>
2022-05-12 17:04:51 +0800

    use plyr for ytplayer

--- src/lib/components/extra/youtube.svelte
+++ src/lib/components/extra/youtube.svelte
@@ -1,4 +1,4 @@
-<script lang="ts">
+<!-- <script lang="ts">
   export let id = undefined
   export let list = undefined
   export let playlist = undefined
@@ -25,4 +25,35 @@
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
     loading="lazy"
     allowfullscreen />
+</div> -->
+<script lang="ts">
+  import { onMount } from 'svelte'
+  import Plyr from 'plyr'
+
+  export let id = undefined
+  export let list = undefined
+  export let playlist = undefined
+  export let start = undefined
+  export let autoplay = false
+  export let disablekb = false
+  export let controls = true
+  export let fs = true
+  export let loop = false
+
+  const src = `https://www.youtube.com/embed/${id}?${list ? `listType=playlist&list=${list}&` : ''}${
+    playlist ? `playlist=${playlist}&` : ''
+  }${start ? `start=${start}` : ''}${autoplay ? 'autoplay=1&' : ''}${disablekb ? 'disablekb=1&' : ''}${
+    controls ? '' : 'controls=0&'
+  }${fs ? '' : 'fs=0&'}${
+    loop ? 'loop=1' : ''
+  }origin=https://kwchang0831.dev&iv_load_policy=3&modestbranding=1&playsinline=1&showinfo=0&rel=0&enablejsapi=1`
+
+  let players
+  onMount(() => {
+    players = Plyr.setup('.js-player')
+  })
+</script>
+
+<div class="plyr__video-embed relative pb-[56.25%] mb-2 js-player">
+  <iframe {src} title="Plyr video player" allowfullscreen allowtransparency loading="lazy" allow="autoplay" />
 </div>
